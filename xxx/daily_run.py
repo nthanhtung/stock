3ed3a4d75eac554163d57417d87588b9f02ffbc1
Mona Lisa <thanhtung211995@gmail.com>
2021-06-28 12:20:05 +0700

    edit beta

--- xxx/daily_run.py
+++ xxx/daily_run.py
@@ -1,7 +1,7 @@
 import xxx.extract.vn_direct as vd
 import xxx.global_var as gv
 import xxx.transform.beta as bt
-import pandas as pd
+import datetime as dt
 
 vd.get_stock_price_year_range(symbols = gv.vn_top_50, start_y = 2021, end_y = 2021)
 
@@ -11,8 +11,15 @@ vd.get_finance_data_year_range(symbols = gv.vn_top_50, start_y = 2021, end_y = 2
 
 ###################
 
-list_date_to_filter = pd.date_range(start= '1995-12-31', end= '2030-12-31', freq='M', name='Date')
-list_date_to_filter = list(map(lambda x: x.strftime('%Y-%m-%d'), list_date_to_filter))
+# list_date_to_filter = pd.date_range(start= '1995-12-31', end= '2030-12-31', freq='M', name='Date')
+# list_date_to_filter = list(map(lambda x: x.strftime('%Y-%m-%d'), list_date_to_filter))
+# x = bt.calculate_beta(list_date_to_filter, 365)
+# x.to_csv("data/dim/dim_stock_beta.csv")
 
-x = bt.calculate_beta(['2021-05-25'], 365)
-x.to_csv("data/dim/dim_stock_beta.csv")
+today = dt.datetime.now().strftime('%Y-%m-%d')
+
+x = bt.calculate_beta([today], 365)
+x.to_csv("data/dim/dim_stock_beta_1_day.csv")
+
+
+
--- xxx/transform/beta.py
+++ xxx/transform/beta.py
@@ -18,7 +18,7 @@ def roll(df, w, list_date_to_filter, **kwargs):
 
     rolled_df = pd.concat({
         row: pd.DataFrame(values, columns=df.columns)
-        for row, values in zip(df.index, a)
+        for row, values in zip(df.index[(w-1):], a)
     })  # create rolling/sliding window things
 
     rolled_df = rolled_df[rolled_df.index.isin(list_date_to_filter, level=0)] # filter date to avoid excess computation
@@ -56,7 +56,6 @@ def calculate_beta(list_date_to_filter, sliding_window):
     df = td.csv_path_to_df('data\stock_price')
     df = df.pivot(index="tradingDate", columns="symbol", values="close")
     df = df[['^VNINDEX'] + list(df.columns)]
-    df = df.sort_index(ascending=False)
     betas = roll(df.pct_change().fillna(0), sliding_window, list_date_to_filter).apply(beta)
     betas.head(5)
     return betas
