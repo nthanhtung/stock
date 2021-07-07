765ca874488f05cb60ed479e4be7d2a810bdbbcf
Mona Lisa <thanhtung211995@gmail.com>
2021-07-07 19:35:51 +0700

    add source data web investion

--- source/xxx/extract/web_investing.py
+++ source/xxx/extract/web_investing.py
@@ -1,32 +1,50 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import os
+import time
 
 
-def get_cophieu68(driver_path: str = "C:/chromedriver_win32/chromedriver.exe", download_path: str = "C:/stock/data", login_url: str = "https://www.cophieu68.vn/account/login.php", stock_url: str = "https://www.cophieu68.vn/export/excelfull.php?id=^vnindex", un: str = "x@gmail.com", pw: str = "******"):
-    '''
-    get_cophieu68 using selenium
-    driver_path = "C:/Users/tung.nguyen/Desktop/0 Project/chromedriver_win32/chromedriver.exe"
-    '''       
-    options = Options()
-    options.add_experimental_option("prefs", {
-    "download.default_directory": download_path,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-    })
+#############test
 
-    driver = webdriver.Chrome(driver_path, options = options)
-    driver.get(login_url)
 
-    # login
-    driver.find_elements(By.NAME, 'username')[1].send_keys(un)
-    driver.find_elements(By.NAME, 'tpassword')[1].send_keys(pw)
-    driver.find_elements(By.NAME, 'login')[0].click()
+options = Options()
+options.add_experimental_option("prefs", {
+"download.default_directory": "C:\\Users\\tung.nguyen\\Desktop\\0 Project\\stock\\data\\vn_investing",
+"download.prompt_for_download": False,
+"download.directory_upgrade": True,
+"safebrowsing.enabled": True
+})
 
-    # download data vn index
-    driver.get(stock_url)
-    return True
+driver = webdriver.Chrome("C:/Users/tung.nguyen/Desktop/0 Project/chromedriver_win32/chromedriver.exe", options = options)
+driver.get("https://vn.investing.com/rates-bonds/vietnam-10-year-bond-yield-historical-data")
+
+#wait & close banner
+time.sleep(20)
+#close banner
+try:
+    driver.find_elements(By.CLASS_NAME, 'popupCloseIcon.largeBannerCloser')[0].click()
+    print("close banner")
+except:
+    print("banner already closed")
+
+#login 
+driver.find_elements(By.CLASS_NAME, 'login.bold')[0].click()
+driver.find_elements(By.ID, 'loginFormUser_email')[0].send_keys("thanhtung211995@gmail.com")
+driver.find_elements(By.ID, 'loginForm_password')[0].send_keys("BkkWwkaL123")
+driver.find_elements(By.CLASS_NAME, 'newButton.orange')[2].click()
+
+
+#select data & download
+
+driver.execute_script("window.scrollTo(0, 699)") 
+driver.find_elements(By.ID, 'datePickerIconWrap')[1].click()
+driver.find_elements(By.ID, 'startDate')[0].clear()
+driver.find_elements(By.ID, 'startDate')[0].send_keys("01/01/2000")
+driver.find_elements(By.ID, 'endDate')[0].clear()
+driver.find_elements(By.ID, 'endDate')[0].send_keys("01/01/2030")
+driver.find_elements(By.ID, 'applyBtn')[0].click()
+driver.find_elements(By.CLASS_NAME, 'newBtn.LightGray.downloadBlueIcon.js-download-data')[0].click()
 
 
