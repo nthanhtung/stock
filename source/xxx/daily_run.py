0764303fc9997f45f44c0dc9d38f561cb60e4d8f
Mona Lisa <thanhtung211995@gmail.com>
2021-07-08 16:38:29 +0700

    add web_investing scrapping and refactor code

--- source/xxx/daily_run.py
+++ source/xxx/daily_run.py
@@ -2,6 +2,7 @@ import xxx.extract.api_vn_direct as vd
 import xxx.global_var as gv
 import xxx.transform.beta_x as bt
 import xxx.transform.vnindex as vni
+import xxx.extract.web_investing as wi
 import datetime as dt
 
 
@@ -24,3 +25,11 @@ x.to_csv(gv.data_path + "data/dim/dim_stock_beta_1_day.csv")
 
 vni.standardize_vn_index_data(gv.data_path)
 vni.xxx()
+
+################
+
+##########
+
+y = wi.web_scapping_vn_investing(driver_path = "C:\\chromedriver_win32\\chromedriver.exe", download_path = "C:\\Users\\tung.nguyen\\Desktop\\0 Project\\stock\\data\\vn_investing", un = "thanhtung211995@gmail.com", pw = "BkkWwkaL123")
+d = y.get_government_bond_data_of_1_country(url = "https://vn.investing.com/rates-bonds/vietnam-10-year-bond-yield-historical-data", download_file_name = "vietnam_government_bond_data_10_years")
+
--- source/xxx/extract/web_investing.py
+++ source/xxx/extract/web_investing.py
@@ -1,50 +1,57 @@
-from time import sleep
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import os
 import time
+from .web_scrapping import *
+import xxx.utility as u
 
+class web_scapping_vn_investing(web_scrapping):
+    def __init__(self, driver_path: str, download_path: str, un: str = "x@gmail.com", pw: str = "******") -> None:
+        #create foler if not exist
+        stg_path = f"{download_path}\\stg"
+        fnd_path = f"{download_path}\\fnd"
+        super().__init__(driver_path=driver_path, download_path=stg_path)
+        self.stg_path = stg_path
+        self.fnd_path = fnd_path
+        self.un = un
+        self.pw = pw
+    
+    def get_government_bond_data_of_1_country(self, url, download_file_name):
+        #create data dirs
+        u.create_dir([self.stg_path,self.fnd_path])
+        #launch browser
+        driver = super().launch_browser()
+        driver.get(url)
+        #wait & close banner
+        time.sleep(20)
+        #close banner
+        try:
+            driver.find_elements(By.CLASS_NAME, 'popupCloseIcon.largeBannerCloser')[0].click()
+            print("close banner")
+        except:
+            print("banner already closed")
+        
+        #login 
+        try:
+            driver.find_elements(By.CLASS_NAME, 'login.bold')[0].click()
+            driver.find_elements(By.ID, 'loginFormUser_email')[0].send_keys(self.un)
+            driver.find_elements(By.ID, 'loginForm_password')[0].send_keys(self.pw)
+            driver.find_elements(By.CLASS_NAME, 'newButton.orange')[2].click()     
+        except:
+            print("already login")
 
-#############test
-
-
-options = Options()
-options.add_experimental_option("prefs", {
-"download.default_directory": "C:\\Users\\tung.nguyen\\Desktop\\0 Project\\stock\\data\\vn_investing",
-"download.prompt_for_download": False,
-"download.directory_upgrade": True,
-"safebrowsing.enabled": True
-})
-
-driver = webdriver.Chrome("C:/Users/tung.nguyen/Desktop/0 Project/chromedriver_win32/chromedriver.exe", options = options)
-driver.get("https://vn.investing.com/rates-bonds/vietnam-10-year-bond-yield-historical-data")
-
-#wait & close banner
-time.sleep(20)
-#close banner
-try:
-    driver.find_elements(By.CLASS_NAME, 'popupCloseIcon.largeBannerCloser')[0].click()
-    print("close banner")
-except:
-    print("banner already closed")
-
-#login 
-driver.find_elements(By.CLASS_NAME, 'login.bold')[0].click()
-driver.find_elements(By.ID, 'loginFormUser_email')[0].send_keys("thanhtung211995@gmail.com")
-driver.find_elements(By.ID, 'loginForm_password')[0].send_keys("BkkWwkaL123")
-driver.find_elements(By.CLASS_NAME, 'newButton.orange')[2].click()
-
-
-#select data & download
-
-driver.execute_script("window.scrollTo(0, 699)") 
-driver.find_elements(By.ID, 'datePickerIconWrap')[1].click()
-driver.find_elements(By.ID, 'startDate')[0].clear()
-driver.find_elements(By.ID, 'startDate')[0].send_keys("01/01/2000")
-driver.find_elements(By.ID, 'endDate')[0].clear()
-driver.find_elements(By.ID, 'endDate')[0].send_keys("01/01/2030")
-driver.find_elements(By.ID, 'applyBtn')[0].click()
-driver.find_elements(By.CLASS_NAME, 'newBtn.LightGray.downloadBlueIcon.js-download-data')[0].click()
-
-
+        #select data & download
+        driver.execute_script("window.scrollTo(0, 699)") 
+        driver.find_elements(By.ID, 'datePickerIconWrap')[1].click()
+        driver.find_elements(By.ID, 'startDate')[0].clear()
+        driver.find_elements(By.ID, 'startDate')[0].send_keys("01/01/2000")
+        driver.find_elements(By.ID, 'endDate')[0].clear()
+        driver.find_elements(By.ID, 'endDate')[0].send_keys("01/01/2030")
+        driver.find_elements(By.ID, 'applyBtn')[0].click()
+        time.sleep(5)
+        #retry download 3 times
+        for i in range(3):
+            driver.find_elements(By.CLASS_NAME, 'newBtn.LightGray.downloadBlueIcon.js-download-data')[0].click()
+            time.sleep(1)
+        #file operation
+        u.copy_latest_file(source_dir = self.stg_path, target_dir = self.fnd_path, new_file_name = download_file_name)
+        u.clean_dir(dirs = [self.stg_path])
+        return driver
